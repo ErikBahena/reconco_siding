cb42dc566223945148f410ace8e3091168e30dc0
ErikB <80177106+ErikBahena@users.noreply.github.com>
2023-07-25 12:06:50 -0700

    contact num

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -256,12 +256,12 @@ export default function Home() {
             </p>
 
             <Link
-              href="#contact"
-              className="mt-4 rounded-sm bg-yellow-300 px-8 py-3 text-lg font-bold text-black
-            transition-all duration-300 hover:bg-yellow-400"
-            scroll={false}
+              href="tel:206-739-1149"
+              className="mt-4 rounded-sm bg-yellow-400 px-8 py-3
+            text-lg font-bold text-black transition-all duration-300 hover:bg-yellow-500"
+              scroll={false}
             >
-              Get a Free Estimate
+              Give Us a Call
             </Link>
           </div>
         </div>
@@ -643,7 +643,6 @@ export default function Home() {
             </p>
 
             {/* Responsive contact form, First Name, Last Name, Email, Phone, Address */}
-
             <form
               className="mt-6 flex flex-col gap-4"
               onSubmit={(e) => {
@@ -776,3 +775,40 @@ const DecorativeDivider = ({ gray }: DecorativeDividerProps) => {
 function numberWithCommas(x: number) {
   return new Intl.NumberFormat().format(x);
 }
+
+//   <div className="flex flex-col gap-4 md:flex-row md:gap-8">
+//   <Link
+//     href="tel:425-555-5555"
+//     className="flex items-center gap-2 text-lg font-semibold hover:cursor-pointer"
+//   >
+//     <svg
+//       xmlns="http://www.w3.org/2000/svg"
+//       viewBox="0 0 24 24"
+//       fill="currentColor"
+//       className="h-6 w-6"
+//     >
+//       <path
+//         fillRule="evenodd"
+//         d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z"
+//         clipRule="evenodd"
+//       />
+//     </svg>
+//     (425)-555-5555
+//   </Link>
+
+//   <Link
+//     href="mailto:reconcosiding@gmail.com"
+//     className="flex items-center gap-2 text-lg font-semibold hover:cursor-pointer"
+//   >
+//     <svg
+//       xmlns="http://www.w3.org/2000/svg"
+//       viewBox="0 0 24 24"
+//       fill="currentColor"
+//       className="h-6 w-6"
+//     >
+//       <path d="M19.5 22.5a3 3 0 003-3v-8.174l-6.879 4.022 3.485 1.876a.75.75 0 01-.712 1.321l-5.683-3.06a1.5 1.5 0 00-1.422 0l-5.683 3.06a.75.75 0 01-.712-1.32l3.485-1.877L1.5 11.326V19.5a3 3 0 003 3h15z" />
+//       <path d="M1.5 9.589v-.745a3 3 0 011.578-2.641l7.5-4.039a3 3 0 012.844 0l7.5 4.039A3 3 0 0122.5 8.844v.745l-8.426 4.926-.652-.35a3 3 0 00-2.844 0l-.652.35L1.5 9.59z" />
+//     </svg>
+//     reconcosiding@gmail.com
+//   </Link>
+// </div>
